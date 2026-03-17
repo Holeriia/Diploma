57119e56bad91371df632553873ef62ab52eab6f
Holeriia <holeriia@inbox.ru>
2026-03-17 13:59:33 +0300

    Подгружаются списки

--- src/main/java/com/company/diploma/security/StudentRole.java
+++ src/main/java/com/company/diploma/security/StudentRole.java
@@ -1,5 +1,11 @@
 package com.company.diploma.security;
 
+import com.company.diploma.entity.Student;
+import com.company.diploma.entity.User;
+import io.jmix.security.model.EntityAttributePolicyAction;
+import io.jmix.security.model.EntityPolicyAction;
+import io.jmix.security.role.annotation.EntityAttributePolicy;
+import io.jmix.security.role.annotation.EntityPolicy;
 import io.jmix.security.role.annotation.ResourceRole;
 import io.jmix.securityflowui.role.annotation.MenuPolicy;
 import io.jmix.securityflowui.role.annotation.ViewPolicy;
@@ -11,4 +17,12 @@ public interface StudentRole {
     @MenuPolicy(menuIds = "StudentProfileView")
     @ViewPolicy(viewIds = "StudentProfileView")
     void screens();
+
+    @EntityAttributePolicy(entityClass = Student.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
+    @EntityPolicy(entityClass = Student.class, actions = EntityPolicyAction.ALL)
+    void student();
+
+    @EntityAttributePolicy(entityClass = User.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
+    @EntityPolicy(entityClass = User.class, actions = EntityPolicyAction.ALL)
+    void user();
 }
