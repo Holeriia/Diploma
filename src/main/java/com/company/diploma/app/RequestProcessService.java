9828e0bfd4739cfac10a3f124c8f27068fd13c90
Holeriia <holeriia@inbox.ru>
2026-04-12 21:43:12 +0300

    заявка идет первому приоритету

--- src/main/java/com/company/diploma/app/RequestProcessService.java
+++ src/main/java/com/company/diploma/app/RequestProcessService.java
@@ -2,6 +2,7 @@ package com.company.diploma.app;
 
 import com.company.diploma.entity.*;
 import io.jmix.core.DataManager;
+import io.jmix.core.FetchPlan;
 import org.flowable.engine.RuntimeService;
 import org.springframework.stereotype.Service;
 
@@ -21,20 +22,31 @@ public class RequestProcessService {
     }
 
     public void startProcess(Request request) {
-
         Request req = dataManager.load(Request.class)
                 .id(request.getId())
-                .fetchPlan("_base")
+                .fetchPlan(plan -> {
+                    plan.addFetchPlan(FetchPlan.BASE);
+                    plan.add("initiator", sp -> sp.add("user", up -> up.add("username")));
+                    plan.add("priorities", sp -> {
+                        sp.addFetchPlan(FetchPlan.BASE);
+                        sp.add("participant", pp -> pp.add("user", up -> up.add("username")));
+                    });
+                })
                 .one();
 
-        Participant initiator = req.getInitiator();
-        User initiatorUser = initiator.getUser();
+        RequestPriority firstPriority = req.getPriorities().stream()
+                .filter(p -> p.getPriorityNumber() != null && p.getPriorityNumber() == 1)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Нет участника с приоритетом №1"));
+
+        User firstAssigneeUser = firstPriority.getParticipant().getUser();
+        User initiatorUser = req.getInitiator().getUser();
 
         req.setStatus(RequestStatus.IN_REVIEW);
 
         Map<String, Object> variables = new HashMap<>();
         variables.put("requestId", req.getId());
-        variables.put("assigneeUsername", initiatorUser);
+        variables.put("assigneeUsername", firstAssigneeUser);
         variables.put("previousAssignee", initiatorUser);
 
         runtimeService.startProcessInstanceByKey(
